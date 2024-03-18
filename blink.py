554548e99c3749121ff017304e818cbf13cbf7c2
Muhammad Aliff Mohamed Anuar <96939727+Moon1100@users.noreply.github.com>
2024-03-18 16:32:55 +0800

    initial commit

---
+++ blink.py
@@ -0,0 +1,13 @@
+from gpiozero import LED
+from time import sleep
+
+# Define the GPIO pin number for the LED
+led_pin = 17
+
+# Create an LED object
+led = LED(led_pin)
+
+# Blink the LED
+while True:
+    led.toggle()  # Toggle the LED on/off
+    sleep(1)      # Wait for 1 second
