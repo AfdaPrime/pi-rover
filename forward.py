b6b70b8497cb5dab2e359660526c5205804104bd
Muhammad Aliff Mohamed Anuar <96939727+Moon1100@users.noreply.github.com>
2024-03-20 08:55:17 +0800

    forward script

--- forward.py
+++ forward.py
@@ -1,8 +1,22 @@
 from gpiozero import Motor
 from time import sleep
 
-# Define the motor
-motor = Motor(forward=17)
-motor.forward()
+# Define the motor with forward and backward pins
+motor = Motor(forward=17, backward=18)
 
+try:
+    # Move the motor forward
+    motor.forward()
+    
+    # Wait for 2 seconds
+    sleep(5)
+    
+    # Move the motor backward
+    motor.backward()
+    
+    # Wait for 2 seconds
+    sleep(5)
 
+finally:
+    # Stop the motor
+    motor.stop()
